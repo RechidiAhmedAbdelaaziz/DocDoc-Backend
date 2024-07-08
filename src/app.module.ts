810a3e01de5895fee0c7efac3ed6a61a3cefc959
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2024-07-08 09:29:07 +0100

    chore: Add dependency on mongo in docker-compose.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {  Module } from '@nestjs/common';
+
+import { AuthModule } from './module/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+
+
+const databaseModule = MongooseModule.forRoot(process.env.MONGO_URI, {
+  dbName: 'DocDoc',
+  connectionFactory: (connection) => {
+    if (connection.readyState === 1) {
+      console.log('Database Connected successfully >> ', connection.name);
+    }
+    connection.on('disconnected', () => {
+      console.log('Database disconnected');
+    });
+    connection.on('error', (error) => {
+      console.log('Database connection failed! for error: ', error);
+    });
+    return connection;
+  },
+});
+
+
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    databaseModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '10h' }
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
